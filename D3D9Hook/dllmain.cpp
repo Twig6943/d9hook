e05cabf36a17952db91ec836a45484713878eb1b
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2026-03-30 21:07:10 +0300

    Update dllmain.cpp

--- D3D9Hook/dllmain.cpp
+++ D3D9Hook/dllmain.cpp
@@ -18,6 +18,46 @@
 typedef HRESULT(_stdcall* EndScene)(LPDIRECT3DDEVICE9 pDevice);
 HRESULT _stdcall hkEndScene(LPDIRECT3DDEVICE9 pDevice);
 
+#include <dinput.h>
+
+extern "C" __declspec(dllexport)
+HRESULT __stdcall proxyDirectInput8Create(
+    HINSTANCE hinst,
+    DWORD dwVersion,
+    REFIID riidltf,
+    LPVOID* ppvOut,
+    LPUNKNOWN punkOuter)
+{
+    using tDirectInput8Create = HRESULT(__stdcall*)(HINSTANCE, DWORD, REFIID, LPVOID*, LPUNKNOWN);
+
+    static HMODULE realDinput8 = nullptr;
+    static tDirectInput8Create pDirectInput8Create = nullptr;
+
+    if (!realDinput8)
+    {
+        char sysPath[MAX_PATH];
+        GetSystemDirectoryA(sysPath, MAX_PATH);
+        strcat_s(sysPath, "\\dinput8.dll");
+
+        realDinput8 = LoadLibraryA(sysPath);
+    }
+
+    if (!realDinput8)
+        return DIERR_GENERIC;
+
+    if (!pDirectInput8Create)
+    {
+        pDirectInput8Create = reinterpret_cast<tDirectInput8Create>(
+            GetProcAddress(realDinput8, "DirectInput8Create")
+        );
+    }
+
+    if (!pDirectInput8Create)
+        return DIERR_GENERIC;
+
+    return pDirectInput8Create(hinst, dwVersion, riidltf, ppvOut, punkOuter);
+}
+
 namespace Globals
 {
     static LPDIRECT3DDEVICE9 PD3D_DEVICE = nullptr;
